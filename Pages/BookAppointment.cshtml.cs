e3e1e432d6c5f05f3d906adbfb54c4fd3c67768d
Echrik <biteme@>
2025-04-02 18:42:26 +0200

    Minimalistic design added and some validation.

--- Pages/BookAppointment.cshtml.cs
+++ Pages/BookAppointment.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Healio.Pages
 {
+    [Authorize(Roles = "patient")]
     public class BookAppointmentModel : PageModel
     {
         public void OnGet()
