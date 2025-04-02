38ea71442ab49f72a3ad1d3a37ed59bc8bea2c46
BenceKocsis <Kocsis Bence@DESKTOP-TQQ6TFN>
2025-04-02 18:33:37 +0200

    Inputs asp-for bindings added

--- Pages/Register.cshtml.cs
+++ Pages/Register.cshtml.cs
@@ -1,3 +1,5 @@
+using Healio.Models;
+using Healio.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,31 @@ namespace Healio.Pages
 {
     public class RegisterModel : PageModel
     {
+        private readonly UserService _userService;
+        [BindProperty]
+        public User User { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+        public RegisterModel(UserService userService)
+        {
+            _userService = userService;
+            User = new User();
+            Password = "";
+        }
+
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var result = await _userService.RegisterUserAsync(User, Password);
+            if (!result)
+            {
+                ModelState.AddModelError(string.Empty, "User with this email already exists.");
+                return Page();
+            }
+            return RedirectToPage("/Index");
         }
     }
 }
