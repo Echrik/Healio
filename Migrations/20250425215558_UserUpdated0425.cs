745bd547069f293d0554eef94152c6566cd671af
BenceKocsis <Kocsis Bence@DESKTOP-TQQ6TFN>
2025-04-25 23:59:12 +0200

    [Healio][feat](migration): update User schema and remove Name from profiles
    Introduce migration `UserUpdated0425` to modify the database schema by removing the `Name` column from `PatientProfiles` and `DoctorProfiles`, and adding a non-nullable `Name` column to the `Users` table. Update the DbContext model snapshot to reflect these changes.

---
+++ Migrations/20250425215558_UserUpdated0425.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Healio.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserUpdated0425 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "PatientProfiles");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "DoctorProfiles");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "PatientProfiles",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "DoctorProfiles",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "")
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
