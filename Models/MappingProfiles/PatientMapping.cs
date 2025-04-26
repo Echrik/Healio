f6cb52183e644d01f55ccdd3d45c035c0e145682
BenceKocsis <Kocsis Bence@DESKTOP-TQQ6TFN>
2025-04-26 03:01:29 +0200

    [Healio][feat](mapping): add mapping profiles for Doctor, Patient, and User
    Introduce new AutoMapper profiles: DoctorMapping, PatientMapping, and UserMapping.
    These profiles establish mappings between domain models and their corresponding DTOs using CreateMap.

---
+++ Models/MappingProfiles/PatientMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Healio.Models.DTO;
+using System;
+
+namespace Healio.Models.MappingProfiles
+{
+    public class PatientMapping : Profile
+    {
+        public PatientMapping()
+        {
+            CreateMap<PatientProfile, PublicPatientDTO>();
+        }
+    }
+}
---
+++ Models/MappingProfiles/UserMapping.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Healio.Models.DTO;
+using System;
+
+namespace Healio.Models.MappingProfiles
+{
+    public class UserMapping : Profile
+    {
+        public UserMapping()
+        {
+            CreateMap<User, PublicUserDTO>();
+        }
+    }
+}
