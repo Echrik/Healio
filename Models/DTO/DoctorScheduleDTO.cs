84b33737fb8e12d35d72088e56dbb955d4c0a7b1
BenceKocsis <Kocsis Bence@DESKTOP-TQQ6TFN>
2025-04-27 22:22:50 +0200

    [Healio][feat](DTO, Mapping): add DoctorScheduleDTO and mapping profile
    Introduce a new `DoctorScheduleDTO` class with properties for `DayOfWeek`, `StartTime`, and `EndTime`.
    Add `DoctorScheduleMapping` profile to facilitate mapping between `DoctorSchedule` and `DoctorScheduleDTO` using AutoMapper.

---
+++ Models/DTO/DoctorScheduleDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Healio.Models.DTO
+{
+    public class DoctorScheduleDTO
+    {
+        public string DayOfWeek { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
---
+++ Models/MappingProfiles/DoctorScheduleMapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using Healio.Models.DTO;
+
+namespace Healio.Models.MappingProfiles
+{
+    public class DoctorScheduleMapping : Profile
+    {
+        public DoctorScheduleMapping()
+        {
+            CreateMap<DoctorSchedule, DoctorScheduleDTO>();
+        }
+    }
+}
