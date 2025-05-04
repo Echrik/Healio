f48e1aed299ce8b96dee45ac546922704cb6651b
BenceKocsis <Kocsis Bence@DESKTOP-TQQ6TFN>
2025-05-04 22:21:18 +0200

    [Healio][feat](appointment): update booking form and logic
    Change form method to POST in BookAppointment.cshtml, add required attribute to TimeSelect dropdown, and remove unused day order dictionary. Introduce UserService dependency in BookAppointmentModel to access user data. Update OnPostAsync to retrieve patient and doctor profiles, construct an Appointment object, and redirect to the index page after booking.

--- Pages/BookAppointment.cshtml.cs
+++ Pages/BookAppointment.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Security.Claims;
 
 namespace Healio.Pages
 {
@@ -11,10 +12,12 @@ namespace Healio.Pages
     public class BookAppointmentModel : PageModel
     {
         private readonly AppointmentService _appointmentService;
+        private readonly UserService _userService;
 
-        public BookAppointmentModel(AppointmentService appointmentService)
+        public BookAppointmentModel(AppointmentService appointmentService, UserService userService)
         {
             _appointmentService = appointmentService;
+            _userService = userService;
         }
 
         public List<User> Doctors { get; set; }
@@ -31,12 +34,14 @@ namespace Healio.Pages
         public void OnGet()
         {
             Doctors = _appointmentService.GetDoctors();
-            
         }
         public async Task<IActionResult> OnPostAsync()
         {
-            Console.WriteLine(SelectedDate.DayOfWeek.ToString());
-            return Page();
+            PatientProfile patient = _userService.GetPatientByUserId(User.FindFirst(ClaimTypes.NameIdentifier).Value);
+            DoctorProfile doctor = _userService.GetDoctorByUserId(SelectedDoctorId);
+            DateTime appointmentDate = new DateTime(SelectedDate.Year, SelectedDate.Month, SelectedDate.Day, SelectedTime.Hour, SelectedTime.Minute, SelectedTime.Second);
+            _appointmentService.BookAppointment(new Appointment { AppointmentDate = appointmentDate, DoctorId = SelectedDoctorId, PatientId = patient.Id, Patient = patient, Doctor = doctor });
+            return RedirectToPage("/Index");
         }
     }
 }
