a35ce2916cf6a7492b8bba357022203e54874862
Echrik <biteme@>
2025-05-05 02:33:02 +0200

    [Healio][feat](DTO, Message): add SendMessageDTO and Message classes
    Introduce SendMessageDTO with ReceiverId and Content properties.
    Add Message class with properties for Id, SenderId, Sender, ReceiverId, Receiver, Content, and SentAt.

---
+++ Models/DTO/SendMessageDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace Healio.Models.DTO
+{
+    public class SendMessageDTO
+    {
+        public int ReceiverId { get; set; }  
+        public string Content { get; set; } = string.Empty;
+    }
+}
