630857e161fc1465a735c9030320edfef766af64
BenceKocsis <Kocsis Bence@DESKTOP-TQQ6TFN>
2025-05-05 17:38:46 +0200

    [Healio][feat](HealthData): add HealthDataService for health data management
    Remove folder reference in csproj, add HealthDataService for recording and retrieving health data. Update HealthDataCheckoutModel to use the new service. Remove RecordHealthData method from AppointmentService.

---
+++ Services/HealthDataService.cs
@@ -0,0 +1,24 @@
+﻿using Healio.Models;
+
+namespace Healio.Services
+{
+    public class HealthDataService
+    {
+        private readonly HealioDbContext _context;
+        public HealthDataService(HealioDbContext context)
+        {
+            _context = context;
+        }
+        public bool RecordHealthData(HealthData healthData)
+        {
+            if (_context.HealthData.Contains(healthData))
+                return false;
+            _context.HealthData.Add(healthData);
+            return _context.SaveChanges() > 0;
+        }
+        public List<HealthData> GetHealthData(int id)
+        {
+            return _context.HealthData.Where(h => h.Patient.UserId == id).ToList();
+        }
+    }
+}
